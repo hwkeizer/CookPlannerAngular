473fa76f6f3502e78133be4ffca2f326e12ba809
Henk Keizer <hwkeizer@telfort.nl>
2019-05-23 08:36:12 +0200

    Implemented addition and removal of ingredients

--- src/app/component/ingredient/ingredient-list-edit/ingredient-list-edit.component.ts
+++ src/app/component/ingredient/ingredient-list-edit/ingredient-list-edit.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
 import { Ingredient } from 'src/app/model/Ingredient';
-import { IngredientName } from 'src/app/model/IngredientName';
 
 /**
  * Component to display an editable list of ingredients in a table
@@ -25,39 +24,52 @@ export class IngredientListEditComponent implements OnInit {
     this.ingredientsForm = this.formBuilder.group({
       ingredients: new FormArray([])
     });
-
     // Add the ingredients to the ingredients FormArray
     this.ingredients.map((o, i) => {
-      const control = this.formBuilder.group(o);
-      (this.ingredientsForm.controls.ingredients as FormArray).push(control);
+      this.ingredientToGroup(o);
     })
-
     // link the ingredientsForm to the parent's form (recipeForm)
     this.formReady.emit(this.ingredientsForm);
   }
 
-  // Add updated measureUnit to the ingredient and patch into the form
-  updateMeasureUnit(measureUnit, ingredient) {
-    this.ingredients = this.ingredientsForm.get('ingredients').value;
-    this.ingredients.map((item, i) => {
-      if (item.id == ingredient.id) {
-        this.ingredients[i].measureUnit = measureUnit;
-      }
+  ingredientToGroup(ingredient: Ingredient) {
+    const formGroup = this.formBuilder.group({
+      id: [ingredient.id || ''],
+      name: [ingredient.name || ''],
+      amount: [ingredient.amount || '', Validators.required],
+      stock: [ingredient.stock || ''],
+      measureUnit: [ingredient.measureUnit || '']
     });
+    (this.ingredientsForm.controls.ingredients as FormArray).push(formGroup);
+  }
+
+  addIngredient() {
+    const ingredient = new Ingredient();   
+    this.ingredientToGroup(ingredient);
+    this.ingredients.push(ingredient);
+  }
+
+  removeIngredient(ingredientIndex) {
+    this.ingredients.splice(ingredientIndex, 1);
+    (this.ingredientsForm.controls.ingredients as FormArray).removeAt(ingredientIndex);
+  
+    // this.ingredientsForm.get('ingredients').setValue(this.ingredients);
+  }
+
+  // Add updated measureUnit to the ingredient and patch into the form
+  updateMeasureUnit(measureUnit, ingredientIndex) {
+    this.ingredients = this.ingredientsForm.get('ingredients').value;
+    this.ingredients[ingredientIndex].measureUnit = measureUnit;
     this.ingredientsForm.get('ingredients').patchValue(this.ingredients);
   }
 
   // Add updated ingredientName to the ingredient and patch into the form
-  updateIngredientName(ingredientName, ingredient) {
+  updateIngredientName(ingredientName, ingredientIndex) {
     this.ingredients = this.ingredientsForm.get('ingredients').value;
-    this.ingredients.map((item, i) => {
-      if (item.id == ingredient.id) {
-        this.ingredients[i].name = ingredientName;
-      }
-    });
+    this.ingredients[ingredientIndex].name = ingredientName;
     this.ingredientsForm.get('ingredients').patchValue(this.ingredients);
   }
 
-  
+  get amount() {return this.ingredientsForm.get('ingredients.amount');}
 
 }
